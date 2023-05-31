16f731642a44097e3b1512730359704337dc9fd8
Vladyslav Kucher <Vladyslav_Kucher@epam.com>
2023-05-31 10:54:04 +0300

    Initial spring-boot application

---
+++ src/test/java/ua/kneu/majnoreg/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kneu.majnoreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
